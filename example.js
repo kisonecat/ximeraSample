eb0d466daa8a929a5a3597215350b23277b76293
Jim Fowler <fowler@math.osu.edu>
2016-07-26 21:48:34 -0400

    a sample with interactive

--- example.js
+++ example.js
@@ -1,18 +1,20 @@
 define(['jquery'], function($) {
     var target = this;
     
-    target.css('background-color','#EEE');
     target.width("100%");
     target.height(300);
 
     target.trigger( 'ximera:answer-needed' );
 
     target.persistentData(function(event) {
-	target.css('background-color','#DFE');
+	if (target.persistentData( 'correct'))
+	    target.css('background-color','#DFE');
+	else
+	    target.css('background-color','#EEE');	    
     });
     
     target.click( function() {
-	target.persistentData( 'correct', true);
+	target.persistentData( 'correct', true );
 	target.trigger( 'ximera:correct' );
     });
 });
