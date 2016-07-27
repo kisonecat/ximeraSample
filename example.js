72e4e84e58e4c486ac4db4c90b40edfa3b5a3f13
Jim Fowler <fowler@math.osu.edu>
2016-07-26 21:27:58 -0400

    a sample with interactive

--- example.js
+++ example.js
@@ -1,4 +1,10 @@
 define(['jquery'], function($) {
     alert('alert!');
     console.log( "this=",this );
+
+    var target = $(this);
+
+    target.css('background-color','blue');
+    target.width(100);
+    target.height(100);
 });
